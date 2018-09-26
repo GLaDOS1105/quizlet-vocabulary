9ab6fd024057f1bec9c9e58377a2eb9797cd6a4c
Sean Wu <seanwu1105@gmail.com>
2018-09-27 00:52:39 +0800

    build popup app frame

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,13 +1,40 @@
 <template>
-  <div>
-    <p>Hello World! (Without Vue-Router)</p>
-  </div>
+  <v-app dark>
+    <v-toolbar app dense>
+      <img src="@/assets/logo.png" alt="Vuetify.js" class="logo ma-1">
+      <v-toolbar-title>In-Page Vocabulary</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-menu bottom left>
+        <v-btn slot="activator" icon>
+          <v-icon>more_vert</v-icon>
+        </v-btn>
+        <v-list>
+          <v-list-tile
+          v-for="(item, index) in items"
+          :key="index"
+          :href="item.url"
+          target="_blank"
+          >
+          <v-list-tile-title>{{item.title}}</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+    </v-toolbar>
+    <v-content></v-content>
+    <v-footer app></v-footer>
+  </v-app>
 </template>
 
 <script>
 export default {
   data () {
-    return {}
+    return {
+      items: [
+        { title: 'Settings', url: chrome.extension.getURL('../options/options.html') },
+        { title: 'Rate Extension', url: '#' },
+        { title: 'Bugs Report', url: 'https://github.com/GLaDOS1105/quizlet-vocabulary/issues' }
+      ]
+    }
   }
 }
 </script>
@@ -16,5 +43,9 @@ export default {
 html {
     width: 400px;
     height: 400px;
+}
+.logo {
+  max-width: 70%;
+  max-height: 70%;
 }
 </style>
