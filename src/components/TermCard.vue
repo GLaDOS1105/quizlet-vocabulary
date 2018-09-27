b42eacffcca7def65d0dac1573abcd770bd08b21
Sean Wu <seanwu1105@gmail.com>
2018-09-27 14:50:27 +0800

    build termcard

---
+++ src/components/TermCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-flex xs12>
+    <v-card>
+      <v-container fill-height fluid>
+        <v-layout fill-height>
+          <v-flex xs5>
+            <span class="title">{{term}}</span>
+          </v-flex>
+          <v-divider vertical></v-divider>
+          <v-flex xs7>
+            <span class="subheading">{{definition}}</span>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+export default {
+  name: 'TermCard',
+  props: ['term', 'definition']
+}
+</script>
+
+<style scoped>
+span {
+  word-break: break-all;
+}
+</style>
--- src/popup/App.vue
+++ src/popup/App.vue
@@ -4,13 +4,16 @@
       <img src="@/assets/logo.png" alt="Vuetify.js" class="logo ma-1">
       <v-toolbar-title>In-Page Vocabulary</v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>refresh</v-icon>
+      </v-btn>
       <v-menu bottom left>
         <v-btn slot="activator" icon>
           <v-icon>more_vert</v-icon>
         </v-btn>
         <v-list>
           <v-list-tile
-          v-for="(item, index) in items"
+          v-for="(item, index) in menuItems"
           :key="index"
           :href="item.url"
           target="_blank"
@@ -20,19 +23,39 @@
         </v-list>
       </v-menu>
     </v-toolbar>
-    <v-content></v-content>
-    <v-footer app></v-footer>
+    <v-content>
+      <v-container grid-list-md>
+        <v-layout row wrap>
+          <term-card
+          v-for="(item, index) in termItems"
+          :key="index"
+          :term="item.term"
+          :definition="item.definition"
+          ></term-card>
+        </v-layout>
+      </v-container>
+    </v-content>
   </v-app>
 </template>
 
 <script>
+import TermCard from '../components/TermCard'
+
 export default {
+  components: {
+    TermCard
+  },
   data () {
     return {
-      items: [
+      menuItems: [
         { title: 'Settings', url: chrome.extension.getURL('../options/options.html') },
         { title: 'Rate Extension', url: '#' },
         { title: 'Bugs Report', url: 'https://github.com/GLaDOS1105/quizlet-vocabulary/issues' }
+      ],
+      termItems: [
+        { term: 'why', definition: '為什麼' },
+        { term: 'so', definition: '如此' },
+        { term: 'superfluous', definition: '多餘' }
       ]
     }
   }
@@ -41,8 +64,8 @@ export default {
 
 <style>
 html {
-    width: 400px;
-    height: 400px;
+  width: 400px;
+  min-height: 200px;
 }
 .logo {
   max-width: 70%;
