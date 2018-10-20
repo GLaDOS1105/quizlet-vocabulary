2805f364cfe98b94b6fb0a6593e2cf444bcf7949
Sean Wu <seanwu1105@gmail.com>
2018-10-20 17:59:58 +0800

    finish the foundTerms set

--- src/content_scripts/content-script.js
+++ src/content_scripts/content-script.js
@@ -1,24 +1,50 @@
-'use strict'
+import Mark from 'mark.js'
+
 // TODO: settings - load url list from chrome.storage.sync.get()
 let matchesUrls = [
-  'developer.mozilla.org'
+  'www.google.com'
 ]
-let matchAllUrl = true
+let matchAllUrl = false
 
 // TODO: settings - auto inject?
 // if auto injected -> re-find terms should be fired every time the url changes and content of page changes
 let autoInject = true
 
+// TODO: load dictionary from Quizlet
+let dict = ['the', 'like', 'a']
+
+// Popup will require this set to display.
+let foundTerms = new Set()
+
 if (matchAllUrl || isMatchedUrl()) {
   if (autoInject) {
-    let observer = new MutationObserver((records, observer) => {
-      console.log(findTerms())
+    let marker = new Mark(findTextNodes())
+    let hasMutated = false
+    let observeOptions = { childList: true, characterData: true, subtree: true }
+    let observer = new MutationObserver((_mutations, _observer) => {
+      hasMutated = true
     })
-    observer.observe(document, {
-      childList: true,
-      characterData: true,
-      subtree: true
-    })
+    setInterval(() => {
+      if (hasMutated) {
+        observer.disconnect()
+        console.log('mark')
+        marker.unmark({
+          done: () => {
+            marker = new Mark(findTextNodes())
+            marker.mark(dict, {
+              accuracy: 'exactly',
+              filter: (_textNode, foundTerm, _totalCounter, _counter) => {
+                foundTerms.add(foundTerm)
+                return true
+              }
+            })
+          }
+        })
+        hasMutated = false
+        observer.observe(document, observeOptions)
+      }
+    }, 3000)
+    observer.observe(document, observeOptions)
   }
 }
 
@@ -29,7 +55,7 @@ function isMatchedUrl () {
   return false
 }
 
-function findTerms () {
+function findTextNodes () {
   return Array.from(document.querySelectorAll('body *')).filter(node => {
     if (['SCRIPT', 'STYLE'].includes(node.tagName)) return false
     let style = getComputedStyle(node)
