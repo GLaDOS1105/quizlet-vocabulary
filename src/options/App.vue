e87208d18257ef19bbd5713d33d30a546e87275e
Sean Wu <seanwu1105@gmail.com>
2018-11-13 14:04:26 +0800

    update option dialog

--- src/options/App.vue
+++ src/options/App.vue
@@ -77,7 +77,12 @@
                     :disabled="!autoHighlight || selectAllWebsites"
                   >Add Website</v-btn>
                   <v-card>
-                    <v-card-title class="headline">{{ dialogTitle }}</v-card-title>
+                    <v-card-title
+                      primary-title
+                      class="headline primary white--text"
+                    >
+                      {{ dialogTitle }}
+                    </v-card-title>
                     <v-card-text>
                       <p>If the URL of a website includes one of the following string, this extension would automatically highlight the user-defined terms within the page. (Only in automatically highlighting mode)</p>
                       <v-text-field
@@ -152,6 +157,23 @@
         </v-flex>
       </v-layout>
     </v-container>
+    <v-footer color="primary" height="auto">
+      <v-layout justify-center row wrap>
+        <v-tooltip
+          top
+          v-for="link in footerLinks"
+          :key="link.tooltip"
+        >
+          <v-btn icon slot="activator" @click="link.action">
+            <v-icon color="white">{{ link.icon }}</v-icon>
+          </v-btn>
+          <span>{{ link.tooltip }}</span>
+        </v-tooltip>
+        <v-flex xs12 class="primary lighten-1 py-1 text-xs-center white--text" >
+          &copy;2018 - <strong>NCU Helper</strong>
+        </v-flex>
+      </v-layout>
+    </v-footer>
   </v-app>
 </template>
 
@@ -168,7 +190,24 @@ export default {
       editedWebsite: { url: '' },
       defaultWebsite: { url: '' },
       markingBgColor: '',
-      markingFgColor: ''
+      markingFgColor: '',
+      footerLinks: [{
+        action () { open('') },
+        icon: 'rate_review',
+        tooltip: '評分擴充程式'
+      }, {
+        action () { open('mailto:seanwu1105@gmail.com') },
+        icon: 'email',
+        tooltip: '開發者信箱'
+      }, {
+        action () { open('https://github.com/GLaDOS1105/quizlet-vocabulary/issues') },
+        icon: 'bug_report',
+        tooltip: '錯誤回報'
+      }, {
+        action () { open('https://github.com/GLaDOS1105/quizlet-vocabulary') },
+        icon: 'code',
+        tooltip: 'GitHub'
+      }]
     }
   },
   computed: {
